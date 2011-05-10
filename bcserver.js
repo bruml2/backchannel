0f58fae34da9652fed1a316198767e2e7603c696
Beardsley Ruml <b@ruml.com>
2011-05-10 14:18:07 -0400

    hand-off to Andrew and github

--- bcserver.js
+++ bcserver.js
@@ -4,17 +4,26 @@
  */
 
 /**
- *  BAD BUG: in browser, forgot to issue the socket.connect();
  *  FLASH: Firefox 4 ships with websockets enables but unavailable because
  *         of a security firewall; use "about:config" to set
  *         network.websocket.override-security-block to true;
  *         Safari 5.0.5 works out of the box; 
  *
- *  NOW: client disconnects after about 15 seconds;
+ *  NOW: some transport other than websocket:
+ *       - client disconnects after about 15 seconds;
  *       - connecting with flashsocket"; there's an error in socket.io.js:
  *           self.__flash.getReadyState is not a function  line 1651
  *
  */
+ 
+// messages are objects with one property whose value is the
+// type of message:
+//   { "post":        { "objtype": "post", <other Post properties> } }
+//   { "topPosts":    [ <array of Post objects> ] }
+//   { "recentPosts": [ <array of Post objects> ] }
+//   { "vote":        { "objtype": "vote", "postid": <string>, "direction": <"up"|"down"> } }
+// unique postid assigned at the server when post arrives; format: <meetingid>-<serial-starting-at-1>;
+// although voting changes the vote total displayed locally, this is cosmetic only;
  
 var 
   http = require('http'),
@@ -27,21 +36,17 @@ var
   // path is used by paperboy;
   path     = require('path'),
   paperboy = require('./lib/paperboy'),
-  // used for uniqueId()
+  // could be used for uniqueId(); probably should be part of any project;
   _        = require('./lib/underscore'),
   
-  GLOBALS = { "meetingID": "888", "nextPostID": 1 },
+  GLOBALS = { "meetingID": "888", "nextPostID": 1, "username": "notyetset" },
   PORT = 8080,
   STATIC = path.join(path.dirname(__filename), 'static');
     
 var server = http.createServer(function(request, response){
-  // console.log("REQUEST for " + request.url);
   var ip = request.connection.remoteAddress;
   var pathname = url.parse(request.url).pathname;
   var query    = url.parse(request.url, true).query;
-  // we need to serve:
-  //  /, /bcmodule.html, favicon.ico, arrow_up.png, arrow_dn.png,
-  //   
   switch (pathname){
     case '/':
       response.writeHead(200, {'Content-Type': 'text/html'});
@@ -50,27 +55,20 @@ var server = http.createServer(function(request, response){
         '<div style="margin: 3em auto; padding: 4em; ' +
         '            border: 3px solid blue; ' +
         '            width: 25em; text-align: center;">' +
-        '<form action="/bcmodule.html" method="post">' +
+        // we actually might prefer POST but I don't know how to extract params;
+        '<form action="/bcmodule.html" method="get">' +
         '  <label for="username">User handle: </label>' +
         '    <input type="text" id="username" value="bruml2" /><br /><br />' +
         '    <input type="submit" value="Send" />' +
         '</form></div>'
       );
       response.end();
-      break;
+    break;
     
-    case '/username':
-      // temporary;
-      // will have a querystring of "username=<value>";
-      var query = url.parse(request.url, true).query;
-      GLOBALS['username'] = query['username'];
-
-      break;
-          
     default:
       if (pathname == '/bcmodule.html') {
-        GLOBALS['username'] = query['username'];    
         if (typeof(GLOBALS['username']) != 'undefined') {
+          GLOBALS['username'] = query['username'];    
           console.log('Captured username: "' + GLOBALS['username'] + '"');
         } else {
           console.log('NO USERNAME found');
@@ -98,6 +96,7 @@ var server = http.createServer(function(request, response){
         response.end('404 - file "' + pathname + '" not found on chat server');
         log(404, request.url, ip, err);
       });
+    break;
   } // switch;
 }); // http.createServer();
 
@@ -113,31 +112,13 @@ server.listen(PORT);
 console.log("==============================");
 console.log("Server listening on port " + PORT + "!");
 
-// listen for socket.io traffic on same port as http traffic;
-var serverListener = io.listen(server);
-// this buffer collects all the messages since the start of the server; when a
-//   new client connects, the buffer is sent first thing.
-var allPosts = [];
-// var topPosts = [];
-// var recentPosts = [];
+// crete socket to listen to socket.io traffic on same port as http traffic;
+var socket = io.listen(server);
 
 function getNextUniquePostID() {
   var nextID = GLOBALS["nextPostID"];
   GLOBALS["nextPostID"] += 1;
   return GLOBALS["meetingID"] + "-" + nextID;
-}
-function assembleTopAndRecentPostArrays() {
-  console.log("All Posts:");
-  allPosts.forEach(function(el) { console.log(el.postid + " - " + el.posvotes + " - " + el.created) });
-  /*
-  topPosts = allPosts.sort(sortPostsByVoteRankDescending);
-  console.log("Top Posts:");
-  topPosts.forEach(function(el) { console.log(el.postid + " - " + el.posvotes + " - " + el.created) });
-  
-  recentPosts = allPosts.sort(sortPostsByCreatedDescending);
-  console.log("Recent Posts:");
-  recentPosts.forEach(function(el) { console.log(el.postid + " - " + el.posvotes + " - " + el.created) });
-  */
 }
 function sortPostsByVoteRankDescending(a, b) {
   aRank = a.posvotes - (a.negvotes * 0.5);
@@ -148,6 +129,18 @@ function sortPostsByVoteRankDescending(a, b) {
 function sortPostsByCreatedDescending(a, b) {
   // for descending, reverse usual order; 
   return b.created - a.created;
+}
+function tallyVote(postid, direction) {
+  allPosts.forEach(function(el) {
+    if (el.postid == postid) {
+      switch (direction) {
+        case "up":   el.posvotes += 1; break;
+        case "down": el.negvotes += 1; break;
+        default: console.log("Bad direction on vote: " + el.direction);
+      }
+      // terminate iteration here?
+    }
+  });
 }
 
 // temporary: for testing;
@@ -180,17 +173,23 @@ var dummyPosts = [ {
   isdemoted: false,
   created: (new Date).getTime() + 10000
 } ];
+// allPosts collects all the messages since the start of the server; when a
+// new client connects, the buffer is sent immediately (it's the current state).
+// would need to be per school, per class and per lecture;
+var allPosts = [];
+// prime the pump a little;
 allPosts = dummyPosts;
-// assembleTopAndRecentPostArrays();
   
-serverListener.on('connection', function(client){
+socket.on('connection', function(client){
   // ====> we're in the connection callback: executes once;
   // NB: when the sorting occurred outside the callback, it was not reflected in
   //     the actual order sent;  this way works!  [Why is this?]
   console.log("======== sending topPosts and recentPosts on connection event =======");
   var topPosts = allPosts.sort(sortPostsByVoteRankDescending);
   console.log("Top Posts:");
-  topPosts.forEach(function(el) { console.log(el.postid + " - " + el.posvotes + " - " + el.created) });
+  topPosts.forEach(function(el) {
+    console.log(el.postid + ": " + el.posvotes + "/" + el.negvotes + " - " + el.created) 
+  });
   client.send( { "topPosts": topPosts } );
 
   var recentPosts = allPosts.sort(sortPostsByCreatedDescending);
@@ -201,38 +200,41 @@ serverListener.on('connection', function(client){
   console.log("Sent " + topPosts.length + " accumulated topPosts.");
   console.log("Sent " + recentPosts.length + " accumulated recentPosts.");
   console.log("======== done =======");
-  // sends a message to all other clients; equivalent to:
-  //    Listener::broadcast(message, client.sessionid);
   client.broadcast({ "announcement": 'Hey! ' + client.sessionId + ' connected' });
   
+  // create callbacks for this client;
   client.on('message', function(message){
-    if (message.objtype == "post") {
-      console.log("POST received from client: " + client.sessionId);
-      message.postid = getNextUniquePostID();
-      console.log(message);
-      allPosts.push(message);
-      console.log("now have " + allPosts.length + " posts in all");
+    if ("post" in message) {
+      message.post.postid = getNextUniquePostID();
+      console.log("POST " + message.postid + " received from client: " + client.sessionId);
+      allPosts.push(message.post);
+      console.log("Now have " + allPosts.length + " posts in all");
 
-      client.send( { "announcement": "Server just got a new post" } );
       var topPosts = allPosts.sort(sortPostsByVoteRankDescending);
-      client.send( { "topPosts": topPosts } );
+      client.broadcast( { "topPosts": topPosts } );
+      client.send(      { "topPosts": topPosts } );
       var recentPosts = allPosts.sort(sortPostsByCreatedDescending);
-      client.send( { "recentPosts": recentPosts } );
-      // client.broadcast( { "topPosts": topPosts } );
-      // client.broadcast( { "recentPosts": recentPosts } );
-    } else if (message.objtype == "vote") {
+      client.broadcast( { "recentPosts": recentPosts } );
+      client.send(      { "recentPosts": recentPosts } );
+    } else if ("vote" in message) {
       console.log("VOTE received from client: " + client.sessionId);
       console.log(message);
-      tallyVote(message.postid, message.direction);
-      // code here to send out new top and recent arrays;
-
+      tallyVote(message.vote.postid, message.vote.direction);
+      // only the order of topPosts might change; could check whether it
+      // has before sending traffic; could send a vote message to clients
+      // and display could be changed locally;
+      var topPosts = allPosts.sort(sortPostsByVoteRankDescending);
+      client.broadcast( { "topPosts": topPosts } );
+      client.send(      { "topPosts": topPosts } );
+      var recentPosts = allPosts.sort(sortPostsByCreatedDescending);
+      client.broadcast( { "recentPosts": recentPosts } );
+      client.send(      { "recentPosts": recentPosts } );
     } else {
-      console.log("UNKNOWN message received from client: " + client.sessionId);
+      console.log("UNKNOWN message type (" + message + "); client: " + client.sessionId);
     }
   });
-
   client.on('disconnect', function(){
-    // console.log("Client: " + client.sessionId + " disconnected");
+    console.log("Client: " + client.sessionId + " disconnected");
     client.broadcast({ announcement: client.sessionId + ' disconnected' });
   });
-});
+}); // connection callback;
